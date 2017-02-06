1b80a86a8076823b96b7a39515d5ea8f47ea153f
francois.marot <francois.marot@olea-medical.com>
2017-02-06 15:47:23 +0100

    Added GuiTest marker interface

---
+++ src/main/java/com/teamtter/test/categories/GuiTest.java
@@ -0,0 +1,7 @@
+package com.teamtter.test.categories;
+
+/** Marker interface. Implement this interface to flag some tests as graphical (GUI)
+indicating they will probably occupy your screen, maybe move your mouse and other weird behaviors.
+Thus, such tests should run alone on a computer without any user at the same time. */
+public interface GuiTest {
+}
